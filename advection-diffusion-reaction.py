8872045bb28fe843c2af6502bc3bdaf76cec74c2
Federico Benelli <fedebenelli@outlook.com>
2019-11-06 15:48:09 -0300

    Añadidos comentarios y cálculo de viscosidad de cada parte

--- advection-diffusion-reaction.py
+++ advection-diffusion-reaction.py
@@ -10,40 +10,59 @@ from scipy.integrate import odeint
 kb = 1.38*10**-23
 pi = np.pi
 
-# Defino propiedades
+# Estimación viscosidades
+T = 25 + 273
+
+# Definición de propiedades
 porcentajeAC = 0.06
 dens = 557.82
 densb = 180
 densL = 935.69
-Deff = (1.3*(10**-10))/60
-phi = 2.012453362
-a = 0.000706498099
-visc = 0.01  # Estimado!
-ap = 3.362*10**3
-rad = math.pow(326.5*(3./(4.*math.pi)), 1./3.)
-Dab = kb*293/(6*math.pi*visc*rad)
+Deff = (1.3*(10**-10))/60       
+phi = 2.012453362               # Factor de forma
+a = 0.000706498099              # Longitud característica
+ap = 3.362*10**3                # Área equivalente
 Deq = 6/ap
 
-# Defino parámetros de diseño
-eps = 1.-densb/dens
-L = 4.421
-Dc = 0.2
-A = math.pi*(Dc/2)**2
-S = 1.072*10**-6
-nu = 2.249*10**-6
-tau = 24*3600
+# Propiedades estimadas
+viscH2O = math.e**(-52.843 + 3703.6/T + 5.866*math.log(T) - 5.98*10**(-29)*(T)**10)
+viscEtOH = math.e**(7.875+781.98/T -3.0418*math.log(T)) 
+visc = 0.01
+rad = math.pow(326.5*(3./(4.*math.pi)), 1./3.)                                          # radio molecular de van der waals
+Dab = kb*T/(6*math.pi*visc*rad)                                                         # Ecuación de Einstein para difusividad
+
+# Definición de parámetros de diseño
+eps = 1.-densb/dens     # Porosidad del lecho
+L = 4.421               # Longitud del equipo
+Dc = 0.2                # Diámetro del equipo
+A = math.pi*(Dc/2)**2   # Área del equipo
+S = 1.072*10**-6        # Flujo volumétrico de sólidos (m³/s)
+nu = 2.249*10**-6       # Flujo volumétrico de solvente (m³/s)
+tau = 24*3600           # Tiempo máximo a calcular en la simulación
 
 # Estimación de parámetros
-uz = nu/(A)
-Re = uz*Deq*densL/(visc*eps)
-Pe = 0.2/eps + 0.011/eps + math.pow(eps*Re, 0.48)
-Sc = visc/(densL*Dab)
-Sh = 2+1.1*math.pow(Sc, 0.33)*math.pow(Re, 0.6)
-Dax = Deq*uz/(eps*Pe)
-kL = Sh*Dab/a
+uz = nu/(A)                                         # Velocidad lineal de solvente
+Re = uz*Deq*densL/(visc*eps)                        # Número de Reynolds
+Pe = 0.2/eps + 0.011/eps + math.pow(eps*Re, 0.48)   # Número de Peclet
+Sc = visc/(densL*Dab)                               # Numero de Schmidt
+Sh = 2+1.1*math.pow(Sc, 0.33)*math.pow(Re, 0.6)     # Número de Sherwood
+Dax = Deq*uz/(eps*Pe)                               # Dispersión axial
+kL = Sh*Dab/a                                       # Transferencia de masa en fase líquida
 
+print(f'''Parámetros:
+uz: {uz}
+Re: {Re}
+Pe: {Pe}
+Sc: {Sc}
+Sh: {Sh}
+Dax: {Dax}
+kL: {kL}
+''')
+
+# Cálculo de tiempos de residencia para el líquido y el sólido
 resTime = A*L*eps/nu
 resTimeS = A*L*(1-eps)/S
+
 print('\n\n')
 print(f'Masa de sólido por hora: {round(S*3600*dens,2)} Kg/h')
 print(f'Volumen de solvente por hora: {round(nu*3600*1000,2)} L/h')
@@ -51,6 +70,7 @@ print(f'Volumen de solvente por hora: {round(nu*3600*1000,2)} L/h')
 print(f'Tiempo de residencia de líquido:  {round(resTime/3600,2)}')
 print(f'Tiempo de residencia de sólido: {round(resTimeS/3600,2)}')
 
+# Definición de la constante global de transferencia de masa
 K = 8.07*10**-9
 K = K*ap
 
@@ -60,6 +80,9 @@ K = K*ap
 
 
 def find_nearest(array, value):
+    '''
+    Recibe un array y un valor; devuelve el valor más cercano al recibido
+    '''
     idx = np.searchsorted(array, value, side="left")
     if idx > 0 and (idx == len(array) or math.fabs(value - array[idx-1]) < math.fabs(value - array[idx])):
         return array[idx-1]
@@ -67,6 +90,9 @@ def find_nearest(array, value):
         return array[idx]
 
 def find_nearest_pos(array, value):
+    '''
+    Recibe un array y un valor; devuelve el índice del elemento con el valor más cercano al recibido
+    '''
     idx = np.searchsorted(array, value, side="left")
     if idx > 0 and (idx == len(array) or math.fabs(value - array[idx-1]) < math.fabs(value - array[idx])):
         return idx-1
@@ -75,6 +101,9 @@ def find_nearest_pos(array, value):
 
 # Función de equilibrio
 def eqX(y):
+    '''
+    Devuelve concentración de equilibrio en fase sólida a partir de una concentración en fase líquida
+    '''
     KeqX = [15.80011045, 32.02907632, 5.735438305, 0.053726466, 1.972131456]
     eqX = KeqX[0]*KeqX[1]*y/(1+KeqX[1]*y)+KeqX[2] * \
         KeqX[3]*(y-KeqX[4])/(1-KeqX[3]*(y-KeqX[4]))
@@ -82,6 +111,8 @@ def eqX(y):
 
 # Funciones de transferencia de masa
 def batch(X, t):
+    '''
+    '''
     x = X[0]
     y = X[1]
     dxdt = K * (eqX(y)-x) / (1-eps)
@@ -89,7 +120,8 @@ def batch(X, t):
     return [dxdt, dydt]
 
 def column(F, t):
-
+    '''
+    '''
     x = F[::2]
     y = F[1::2]
     y[0] = 0
@@ -127,7 +159,8 @@ def column(F, t):
     return dFdt
 
 def countercurrent(F, t):
-
+    '''
+    '''
     x = F[::2]
     y = F[1::2]
 
@@ -226,7 +259,7 @@ for i in tiempos:
 
 plt.title('Fase Líquida a distintos tiempos')
 plt.xlabel('Distancia (m)')
-plt.ylabel('Concentración (g/L)')
+plt.ylabel('Concentración (Kg/m³)')
 plt.legend()
 plt.show()
 
@@ -237,15 +270,15 @@ for i in tiempos:
 
 plt.title('Fase Sólida a distintos tiempos')
 plt.xlabel('Distancia (m)')
-plt.ylabel('Concentración (g/L)')
+plt.ylabel('Concentración (Kg/m³)')
 plt.legend()
 plt.show()
 
 # Grafico concentración a la salida del extractor
-plt.plot(t[:], Yout)
+plt.plot(t[:], Yout, label='Fase líquida')
 plt.title('Concentración a la salida')
 plt.xlabel('Tiempo (s)')
-plt.ylabel('Concentración (g/L)')
+plt.ylabel('Concentración (Kg/m³)')
 plt.legend()
 plt.show()
 
@@ -262,10 +295,10 @@ if equip == 'CC':
         rend = 1 - out/(dens*porcentajeAC)
         rendimientoS.append(rend)
 
-    plt.plot(t[:], rendimientoL, label='Rendimiento Líquido')
+    #plt.plot(t[:], rendimientoL, label='Rendimiento Líquido')
     plt.plot(t[:], rendimientoS, label='Rendimiento Sólido')
     plt.xlabel('Tiempo (s)')
-    plt.ylabel('Concentración (g/L)')
+    plt.ylabel('Concentración (Kg/m³)')
 
 if equip == 'SC':
     for i in range(0,tiniPos):
